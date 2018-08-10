b6bb5d74eb018c0e6d4ff8a65bee1cb312ed6c8c
Brad McCollum <btmcco8@gmail.com>
2018-08-10 14:10:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
+require 'pry'
 #write your code here
-
-def countdown
+timer = 30
+def countdown(number)
+  while timer >= 0
+    puts "#{number} SECOND(S)!"
+    timer -= 1
+    if timer == 0
+      return "HAPPY NEW YEAR!"
+    end
+    binding.pry
 end
